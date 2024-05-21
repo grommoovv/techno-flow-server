f7c8d5319bd23c1497a3eaf882b0aeeb34affd6c
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-21 23:41:14 +0300

    refactor: impl New & Run func | Add config params

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,22 +3,27 @@ package server
 import (
 	"context"
 	"net/http"
-	"time"
+	"server-techno-flow/internal/config"
 )
 
 type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string, handler http.Handler) error {
-	s.httpServer = &http.Server{
-		Addr:           ":" + port,
-		Handler:        handler,
-		MaxHeaderBytes: 1 << 20, // 1 MB
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-	}
+func New(config *config.Config, handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:           ":" + config.Server.Port,
+			Handler:        handler,
+			ReadTimeout:    config.Server.ReadTimeout,
+			WriteTimeout:   config.Server.WriteTimeout,
+			MaxHeaderBytes: config.Server.MaxHeaderMegabytes << 20, // 1 MB
 
+		},
+	}
+}
+
+func (s *Server) Run() error {
 	return s.httpServer.ListenAndServe()
 }
 
