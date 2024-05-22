d12a7b20a5aeb5e7b3d6e078da94bf7988b11c95
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-23 00:32:22 +0300

    feat: implemented the logic of user domain handler operation

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,13 +1,107 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"server-techno-flow/internal/domain"
+	"strconv"
+)
 
-func (h *Handler) CreateUser(c *gin.Context) {}
+func (h *Handler) CreateUser(c *gin.Context) {
+	var userDto domain.User
 
-func (h *Handler) GetUser(c *gin.Context) {}
+	if err := c.BindJSON(&userDto); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
 
-func (h *Handler) GetAllUsers(c *gin.Context) {}
+	id, err := h.services.User.CreateUser(userDto)
 
-func (h *Handler) DeleteUser(c *gin.Context) {}
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
 
-func (h *Handler) UpdateUser(c *gin.Context) {}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+}
+
+func (h *Handler) GetUserByUsername(c *gin.Context) {
+	user, err := h.services.User.GetUserByUsername(c.Param("username"))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"user": user,
+	})
+}
+
+func (h *Handler) GetAllUsers(c *gin.Context) {
+	users, err := h.services.User.GetAllUsers()
+
+	if err != nil {
+		fmt.Printf("Error Fetching Users: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"users": users,
+	})
+}
+
+func (h *Handler) DeleteUser(c *gin.Context) {
+	fmt.Printf("DeleteUser Called, param: %s\n", c.Param("id"))
+	paramId := c.Param("id")
+
+	id, err := strconv.Atoi(paramId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	_, err = h.services.User.DeleteUser(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"user_id": id,
+	})
+}
+
+func (h *Handler) UpdateUser(c *gin.Context) {
+	fmt.Printf("UpdateUser Called, param: %s\n", c.Param("id"))
+	paramId := c.Param("id")
+
+	id, err := strconv.Atoi(paramId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	var userUpdateDto domain.UserUpdateDto
+
+	if err := c.BindJSON(&userUpdateDto); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	errUpdate := h.services.User.UpdateUser(id, userUpdateDto)
+
+	if errUpdate != nil {
+		fmt.Printf("Error Updating User: %v\n", errUpdate)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": errUpdate.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+}
