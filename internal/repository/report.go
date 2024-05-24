42a8e27007ef7397fed8d801367344572965fc36
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 16:29:11 +0300

    feat: impl report repo logic

--- internal/repository/report.go
+++ internal/repository/report.go
@@ -1,7 +1,10 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/jmoiron/sqlx"
+	"server-techno-flow/internal/database/postgres"
+	"server-techno-flow/internal/domain"
 )
 
 type ReportRepository struct {
@@ -12,12 +15,45 @@ func NewReportRepository(db *sqlx.DB) *ReportRepository {
 	return &ReportRepository{db: db}
 }
 
-func (rr *ReportRepository) CreateReport() {}
+func (rr *ReportRepository) CreateReport(dto domain.ReportCreateDto) (int, error) {
+	var id int
 
-func (rr *ReportRepository) GetReport() {}
+	query := fmt.Sprintf("INSERT INTO %s (message, user_id, equipment_id) values ($1, $2, $3) RETURNING id", postgres.ReportsTable)
 
-func (rr *ReportRepository) GetAllReports() {}
+	row := rr.db.QueryRow(query, dto.Message, dto.UserId, dto.EquipmentId)
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
 
-func (rr *ReportRepository) DeleteReport() {}
+	return id, nil
+}
+
+func (rr *ReportRepository) GetAllReports() ([]domain.Report, error) {
+	var reports []domain.Report
+	query := fmt.Sprintf("SELECT * FROM %s ORDER BY id ASC", postgres.ReportsTable)
+	if err := rr.db.Select(&reports, query); err != nil {
+		return nil, err
+	}
+	return reports, nil
+}
+
+func (rr *ReportRepository) GetReportById(id int) (domain.Report, error) {
+	var report domain.Report
+
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id = $1", postgres.ReportsTable)
+
+	err := rr.db.QueryRow(query, id).Scan(&report.Id, &report.Message, &report.CreatedAt, &report.UserId, &report.EquipmentId)
+
+	return report, err
+}
+
+func (rr *ReportRepository) DeleteReport(id int) (int, error) {
+	query := fmt.Sprintf("DELETE FROM %s WHERE id=$1", postgres.ReportsTable)
+	if _, err := rr.db.Exec(query, id); err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
 
 func (rr *ReportRepository) UpdateReport() {}
