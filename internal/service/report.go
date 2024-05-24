0e3c8cc75b500129ff0e58c46e21587b474308ad
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 16:30:35 +0300

    feat: impl report service logic

--- internal/service/report.go
+++ internal/service/report.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"server-techno-flow/internal/domain"
 	"server-techno-flow/internal/repository"
 )
 
@@ -12,12 +13,20 @@ func NewReportService(repo repository.Report) *ReportService {
 	return &ReportService{repo: repo}
 }
 
-func (rs *ReportService) CreateReport() {}
+func (rs *ReportService) CreateReport(dto domain.ReportCreateDto) (int, error) {
+	return rs.repo.CreateReport(dto)
+}
 
-func (rs *ReportService) GetReport() {}
+func (rs *ReportService) GetAllReports() ([]domain.Report, error) {
+	return rs.repo.GetAllReports()
+}
 
-func (rs *ReportService) GetAllReports() {}
+func (rs *ReportService) GetReportById(id int) (domain.Report, error) {
+	return rs.repo.GetReportById(id)
+}
 
-func (rs *ReportService) DeleteReport() {}
+func (rs *ReportService) DeleteReport(id int) (int, error) {
+	return rs.repo.DeleteReport(id)
+}
 
 func (rs *ReportService) UpdateReport() {}
