597954ff5c7bad17ff95b11523da8f002981e335
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 15:52:09 +0300

    feat: impl equipment service logic

--- internal/service/equipment.go
+++ internal/service/equipment.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"server-techno-flow/internal/domain"
 	"server-techno-flow/internal/repository"
 )
 
@@ -12,12 +13,22 @@ func NewEquipmentService(repo repository.Equipment) *EquipmentService {
 	return &EquipmentService{repo: repo}
 }
 
-func (es *EquipmentService) CreateEquipment() {}
+func (es *EquipmentService) CreateEquipment(dto domain.EquipmentCreateDto) (int, error) {
+	return es.repo.CreateEquipment(dto)
+}
 
-func (es *EquipmentService) GetEquipment() {}
+func (es *EquipmentService) GetAllEquipment() ([]domain.Equipment, error) {
+	return es.repo.GetAllEquipment()
+}
 
-func (es *EquipmentService) GetAllEquipment() {}
+func (es *EquipmentService) GetEquipmentById(id int) (domain.Equipment, error) {
+	return es.repo.GetEquipmentById(id)
+}
 
-func (es *EquipmentService) DeleteEquipment() {}
+func (es *EquipmentService) DeleteEquipment(id int) (int, error) {
+	return es.repo.DeleteEquipment(id)
+}
 
-func (es *EquipmentService) UpdateEquipment() {}
+func (es *EquipmentService) UpdateEquipment(id int, dto domain.EquipmentUpdateDto) error {
+	return es.repo.UpdateEquipment(id, dto)
+}
