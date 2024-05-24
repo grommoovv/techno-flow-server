765fb32972c3ecf9d959558d48a8b998e806a5c4
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 15:41:52 +0300

    refactor: edit struct

--- internal/domain/maintenance.go
+++ internal/domain/maintenance.go
@@ -1,9 +1,11 @@
 package domain
 
+import "time"
+
 type Maintenance struct {
-	Id          int
-	StartDate   string
-	EndDate     string
-	Timeline    string
-	EquipmentId int
+	Id          int       `json:"id" db:"id"`
+	StartDate   time.Time `json:"start_date" db:"start_date"`
+	EndDate     time.Time `json:"end_date" db:"end_date"`
+	Timeline    string    `json:"timeline"`
+	EquipmentId int       `json:"equipment_id" db:"equipment_id"`
 }
