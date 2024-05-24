3efa88e0040a827a9768ca9fc8cdd3720ac9b706
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 15:45:25 +0300

    feat: impl sign-in repository logic

--- internal/repository/auth.go
+++ internal/repository/auth.go
@@ -1,7 +1,10 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/jmoiron/sqlx"
+	"server-techno-flow/internal/database/postgres"
+	"server-techno-flow/internal/domain"
 )
 
 type AuthRepository struct {
@@ -12,8 +15,14 @@ func NewAuthRepository(db *sqlx.DB) *AuthRepository {
 	return &AuthRepository{db: db}
 }
 
-func (ar *AuthRepository) SignIn() {}
+func (ar *AuthRepository) SignIn(userDto domain.UserSignInDto) (domain.User, error) {
+	var user domain.User
 
-func (ar *AuthRepository) SignUp() {}
+	query := fmt.Sprintf("SELECT id, username, email, fullname, created_at FROM %s WHERE username = $1 AND password = $2", postgres.UsersTable)
+
+	err := ar.db.QueryRow(query, userDto.Username, userDto.Password).Scan(&user.Id, &user.Username, &user.Email, &user.FullName, &user.CreatedAt)
+
+	return user, err
+}
 
 func (ar *AuthRepository) SignOut() {}
