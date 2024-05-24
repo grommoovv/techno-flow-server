6e2b16b7bf63021064acf9477d7b700d04a094f3
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 15:52:19 +0300

    feat: impl equipment auth logic

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -1,6 +1,9 @@
 package service
 
-import "server-techno-flow/internal/repository"
+import (
+	"server-techno-flow/internal/domain"
+	"server-techno-flow/internal/repository"
+)
 
 type AuthService struct {
 	repo repository.Auth
@@ -10,8 +13,8 @@ func NewAuthService(repo repository.Auth) *AuthService {
 	return &AuthService{repo: repo}
 }
 
-func (as *AuthService) SignIn() {}
-
-func (as *AuthService) SignUp() {}
+func (as *AuthService) SignIn(user domain.UserSignInDto) (domain.User, error) {
+	return as.repo.SignIn(user)
+}
 
 func (as *AuthService) SignOut() {}
