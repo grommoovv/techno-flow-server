24325b93966f167fdedd21e45d6f9f470a1201a2
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 15:41:47 +0300

    refactor: edit struct

--- internal/domain/event.go
+++ internal/domain/event.go
@@ -1,11 +1,14 @@
 package domain
 
+import "time"
+
 type Event struct {
-	Id        int
-	Title     string
-	Type      string
-	StartDate string
-	EndDate   string
-	Status    string
-	UserId    int
+	Id        int       `json:"id" db:"id"`
+	Title     string    `json:"title"`
+	Type      string    `json:"type"`
+	StartDate time.Time `json:"start_date" db:"start_date"`
+	EndDate   time.Time `json:"end_date" db:"end_date"`
+	Duration  time.Time `json:"duration"`
+	Status    string    `json:"status"`
+	UserId    int       `json:"user_id" db:"user_id"`
 }
