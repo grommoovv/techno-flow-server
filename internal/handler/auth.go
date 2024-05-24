9374a786e99c4f8b3beb44fce9ee34e31c3662b0
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 15:42:33 +0300

    feat: impl sign-in handler

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,9 +1,39 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"server-techno-flow/internal/domain"
+)
 
-func (h *Handler) signUp(c *gin.Context) {}
+func (h *Handler) signIn(c *gin.Context) {
+	fmt.Println("signIp called")
 
-func (h *Handler) signIn(c *gin.Context) {}
+	var signInDto domain.UserSignInDto
 
-func (h *Handler) signOut(c *gin.Context) {}
+	if err := c.BindJSON(&signInDto); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error sign in dto": err.Error()})
+		return
+	}
+
+	fmt.Println(signInDto)
+
+	user, err := h.services.Auth.SignIn(signInDto)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error while signing in": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"signed in user": user,
+	})
+}
+
+func (h *Handler) signOut(c *gin.Context) {
+	fmt.Println("signOut called")
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": "signed out",
+	})
+}
