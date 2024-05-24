b9c637b18d88cbaa58c08c513455f5c0d87d1262
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-24 15:41:57 +0300

    refactor: edit struct

--- internal/domain/report.go
+++ internal/domain/report.go
@@ -1,9 +1,11 @@
 package domain
 
+import "time"
+
 type Report struct {
-	Id          int
-	Message     string
-	CreatedAt   string
-	UserId      string
-	EquipmentId int
+	Id          int       `json:"id" db:"id"`
+	Message     string    `json:"message"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UserId      int       `json:"user_id" db:"user_id"`
+	EquipmentId int       `json:"equipment_id" db:"equipment_id"`
 }
